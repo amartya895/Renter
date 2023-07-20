0be6354e77a92374b99912b20c7ccfe325cc9f9d
Amartya Sen <amartyasen895@gmail.com>
2023-07-21 00:01:25 +0530

    booking page

--- server/routes/bookingRoute.js
+++ server/routes/bookingRoute.js
@@ -1,51 +1,70 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const Booking = require('../modals/booking');
-const Hotel = require('../modals/hotel');
+const Booking = require("../modals/booking");
+const Hotel = require("../modals/hotel");
 
-router.post('/bookhotel' , async(req , resp)=>{
+router.post("/bookhotel", async (req, resp) => {
+  const {
+    guestname,
+    guestemail,
+    guestnumber,
+    userid,
+    hotel,
+    fromdate,
+    todate,
+    subtotal,
+    totaldays,
+  } = req.body;
 
-    const {guestname , guestemail , guestnumber , userid , hotel , fromdate , todate ,subtotal , totaldays} = req.body;
-
-    try {
-        const newBooking = new Booking({
-         guestname : guestname,
-         guestemail : guestemail,
-         guestnumber : guestnumber,
-         userid : userid,
-         hotelname : hotel.name,
-         hotelid : hotel._id,
-         fromdate : fromdate,
-         todate :todate,
-         subtotal : subtotal,
-         transactionid : "xcd8479345943350843",
-        });
-
-        const booking = await newBooking.save();
-        console.log(booking);
-
-        const hotelTemp = await Hotel.findOne({ _id: hotel._id });
-        // console.log(hotelTemp);
-        hotelTemp.currentbooking.push({
-            bookingid: booking._id,
-            fromdate: fromdate,
-            todate: todate,
-            userid: userid,
-            status: booking.status,
-          });
-      
-          await hotelTemp.save();
-    
-        resp.send("Your Payment successfull, Booked Successfully");
-      } catch (error) {
-        return resp.status(400).json({ error });
-      }
+  try {
+    const newBooking = new Booking({
+      guestname: guestname,
+      guestemail: guestemail,
+      guestnumber: guestnumber,
+      userid: userid,
+      hotelname: hotel.name,
+      hotelid: hotel._id,
+      fromdate: fromdate,
+      todate: todate,
+      subtotal: subtotal,
+      transactionid: "xcd8479345943350843",
     });
 
+    const booking = await newBooking.save();
+    console.log(booking);
+
+    const hotelTemp = await Hotel.findOne({ _id: hotel._id });
+    hotelTemp.currentbooking.push({
+      bookingid: booking._id,
+      fromdate: fromdate,
+      todate: todate,
+      userid: userid,
+      status: booking.status,
+    });
+
+    await hotelTemp.save();
+
+    resp.send("Your Payment successfull, Booked Successfully");
+  } catch (error) {
+    return resp.status(400).json({ error });
+  }
+});
+
+router.post('/getbooking', async (req, resp) => {
+  const userId = req.body.userid;
+  console.log(userId);
+
+  try {
+    const bookingtemp = await Booking.find({userid : userId});
     
+    console.log(bookingtemp);
 
-
+    return  resp.json(bookingtemp);;
+  } catch (error) {
+    console.log("something went wrong in finding hotel by id");
+  }
+});
 
 module.exports = router;
