9a5ed7710dcb4548fe5a6fbbc30f0acfc345f70e
Amartya Sen <amartyasen895@gmail.com>
2023-08-07 23:02:05 +0530

    user update added

--- server/modals/user.js
+++ server/modals/user.js
@@ -13,6 +13,24 @@ const userSchema = mongoose.Schema({
         type:String,
         required:true,
     },
+    gender : {
+        type : String,
+    },
+    martialstatus :{
+        type : String,
+    },
+    address :{
+        type : String,
+    },
+    dob : {
+        type : String,
+    },
+    state : {
+        type :String,
+    },
+    pincode :{
+        type : String,
+    },
     isAdmin:{
         type:Boolean,
         default:false
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,42 +1,82 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const User = require('../modals/user');
+const User = require("../modals/user");
 
-router.post('/signup', async(req ,resp)=>{
+router.post("/signup", async (req, resp) => {
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-    const newUser = new User({name:req.body.name , email:req.body.email , password:req.body.password});
-
-    try {
-        const user = await newUser.save();
-        resp.send('User Registerd Successfully');
-    } catch (error) {
-        return resp.status(400).json({message : 'something went wrong'});
-    }
-
+  try {
+    const user = await newUser.save();
+    resp.send("User Registerd Successfully");
+  } catch (error) {
+    return resp.status(400).json({ message: "something went wrong" });
+  }
 });
 
-router.post('/login', async(req , resp)=>{
-    const { email, password } = req.body; // array destructuring 
-    try {
-        const user = await User.findOne({email , password});
-        if(user)
-        {
-            const temp  = {
-                name: user.name,
-                email : user.email,
-                _id : user._id
-            }
+router.post("/login", async (req, resp) => {
+  const { email, password } = req.body; // array destructuring
+  try {
+    const user = await User.findOne({ email, password });
+    if (user) {
+      const temp = {
+        name: user.name,
+        email: user.email,
+        _id: user._id,
+      };
 
-            resp.send(temp);
-        }
-        else {
-            resp.status(400).json({ message: 'Login failed' });
-          }
-    } catch (error) {
-        resp.status(500).json({ error: 'Internal Server Error' });
+      resp.send(temp);
+    } else {
+      resp.status(400).json({ message: "Login failed" });
     }
-})
+  } catch (error) {
+    resp.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-  
-module.exports = router
+router.post("/updateuser", async (req, resp) => {
+  const {
+    userid,
+    username,
+    usergender,
+    usermartial,
+    useraddress,
+    userdob,
+    userstate,
+    userpincode,
+  } = req.body;
+
+  try {
+    const user = await User.findOne({ _id: userid }); 
+    if (!user) {
+      return resp.status(404).json({ message: "User not found" });
+    }
+
+    console.log(user);
+    
+    user.name = username;
+    user.gender = usergender;
+    user.martialstatus = usermartial;
+    user.address = useraddress;
+    user.dob = userdob;
+    user.state = userstate;
+    user.pincode = userpincode;
+
+    await user.save(); 
+
+    console.log("Updated user:", user);
+    return resp.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    console.error("Error updating user:", error);
+    return resp.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
+
+
+module.exports = router;
