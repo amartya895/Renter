2236d24788e3d9d5f3862f19784e72bcc4c6b0e4
Amartya Sen <amartyasen895@gmail.com>
2023-08-09 22:26:07 +0530

    updated vercel domain

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://vercel.com', // Replace with your actual frontend domain
+  origin: 'https://renter-khaki.vercel.app/', // Replace with your actual frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
