059994361f94568ffe000a6f5665864444d2161c
Amartya Sen <amartyasen895@gmail.com>
2023-08-10 14:54:02 +0530

    added traveller

--- renter-client/src/components/AddTravellerModal.js
+++ renter-client/src/components/AddTravellerModal.js
@@ -31,8 +31,8 @@ const AddTravellerModal = ({ closeModal }) => {
     console.log(data);
     try {
       const res = await axios.post(
-        // "https://renter-backend.onrender.com/api/travellers/addtraveller",
-        "/api/travellers/addtraveller",
+        "https://renter-backend.onrender.com/api/travellers/addtraveller",
+        // "/api/travellers/addtraveller",
         data
       );
       console.log("Data sent Successfully");
